5b1781ff6a27f4b3fa4b8198142c3dccab757f12
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-04-06 07:48:55 -0400

    Remove helper.ps1

--- s2svpn-winras/helper.ps1
+++
@@ -1,3 +0,0 @@
-# Get-AzPublicIpAddress -ResourceGroupName hub-rg | Where name -like '*vpng-pip' | Select -ExpandProperty ipaddress
-# Get-AzVirtualNetwork -ResourceGroupName hub-rg | select -ExpandProperty AddressPrefix 
-# az network vnet list --resource-group hub-rg
