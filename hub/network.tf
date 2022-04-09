81ba4d0d4ecf62abdd102904120639a6e42e9f63
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-04-08 23:33:21 -0400

    add dns server to hub

--- hub/network.tf
+++ hub/network.tf
@@ -4,6 +4,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = var.vnet_address_space
+  dns_servers         = [ var.dns_vm_001_private_ip_address ]
 }
 
 # Subnet: AzureFirewallSubnet must be /26
