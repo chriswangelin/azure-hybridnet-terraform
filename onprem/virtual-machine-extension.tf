8b220fff047e1fb22b96f70edfe6031ee29fff6b
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-04-10 15:54:16 -0400

    Add 2nd vm extension to onprem dns vm to restart network manager

--- onprem/virtual-machine-extension.tf
+++ onprem/virtual-machine-extension.tf
@@ -1,7 +1,6 @@
-# See https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
-# Run "base64" command on "install-docker.sh" to get encoded value for script parameter
-resource "azurerm_virtual_machine_extension" "dns_vm_001_customscript_vmext" {
-  name                 = "dns_vm_001_customscript"
+# This vm extension must execute before setting the DNS servers on the onprem vnet.
+resource "azurerm_virtual_machine_extension" "dns_vm_001_config_unbound_dns_customscript_vmext" {
+  name                 = "dns_vm_001_config_unbound_dns_customscript"
   virtual_machine_id   = module.dns_vm_001.vm_id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
@@ -13,4 +12,23 @@ resource "azurerm_virtual_machine_extension" "dns_vm_001_customscript_vmext" {
     "commandToExecute": "bash ./config-unbound-dns-onprem-rhel.sh"
   }
 SETTINGS
+}
+
+# After DNS servers are set on the vnet, restart NetworkManager to update /etc/resolv.conf.
+resource "azurerm_virtual_machine_extension" "dns_vm_001_restart_network_manager_customscript_vmext" {
+  name                 = "dns_vm_001_restart_network_manager_customscript"
+  virtual_machine_id   = module.dns_vm_001.vm_id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+  {
+     "fileUris": ["https://raw.githubusercontent.com/chriswangelin/azure-lab-jumpstart/develop/onprem/scripts/restart-network-manager.sh"],
+    "commandToExecute": "bash ./restart-network-manager.sh"
+  }
+SETTINGS
+  depends_on = [
+    azurerm_virtual_network_dns_servers.vnet_dns_servers
+  ]
 }
