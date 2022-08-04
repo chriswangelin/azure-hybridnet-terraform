113412e64afb57ed702dcec535d76ab991697044
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-08-04 19:22:38 -0400

    refactor: refactor again

---
+++ modules/hybridnet/landing-zone/private-dns-zone-link.tf
@@ -0,0 +1,8 @@
+# Private DNS zone in hub for registering virtual machines attached to landing zone vnet
+resource azurerm_private_dns_zone_virtual_network_link vmreg_pdnsz_link {
+  name                  = local.vmreg_pdnsz_link_name
+  resource_group_name   = var.vmreg_pdnsz_resource_group_name
+  private_dns_zone_name = var.vmreg_pdnsz_name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = true
+}
