80d43316f3ee6a3f7c8f9cee82b4ae2790cf2bbb
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-08-03 23:15:13 -0400

    vm-win: conditional output

--- modules/core/virtual-machine-windows/outputs.tf
+++ modules/core/virtual-machine-windows/outputs.tf
@@ -1,14 +1,15 @@
 output vm_id {
   description = "Virtual machine resource ID."
-  value = azurerm_windows_virtual_machine.vm.id
+  value       = azurerm_windows_virtual_machine.vm.id
 }
 
 output private_ip_address {
   description = "Virtual machine private IP address."
-  value = azurerm_network_interface.nic_001.private_ip_address
+  value       = azurerm_network_interface.nic_001.private_ip_address
 }
 
 output public_ip_address {
+  count       = var.enable_public_ip ? 1 : 0
   description = "Virtual machine public IP address."
-  value = azurerm_public_ip.pip_001[0].ip_address
+  value       = azurerm_public_ip.pip_001[0].ip_address
 }
