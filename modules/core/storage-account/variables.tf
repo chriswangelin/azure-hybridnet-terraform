6c7c469c71631f0f872c037aac79fd0fe7d98613
Chris Wangelin <100162194+chriswangelin@users.noreply.github.com>
2022-08-06 22:14:18 -0400

    feat: storage account public network access

--- modules/core/storage-account/variables.tf
+++ modules/core/storage-account/variables.tf
@@ -18,3 +18,9 @@ variable snet_id {
   type        = string
   description = "Resource ID of subnet to which the private endpoint will be connected."
 }
+
+variable storage_account_network_rules_default_action {
+  type        = string
+  description = "Allow or Deny public network access to the storage account."
+  default     = "Deny"
+}
